02ad2ac595531c643c6c134f0daa2a2b7760ae5f
mazeday <mazeday54@gmail.com>
2019-12-23 12:40:15 +0700

    task

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task37/task3702/male/MaleFactory.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task37/task3702/male/MaleFactory.java
@@ -1,4 +1,14 @@
 package com.javarush.task.task37.task3702.male;
 
-public class MaleFactory {
+import com.javarush.task.task37.task3702.AbstractFactory;
+import com.javarush.task.task37.task3702.Human;
+
+public class MaleFactory implements AbstractFactory {
+    public Human getPerson(int age){
+        if (age <= KidBoy.MAX_AGE)
+            return new KidBoy();
+        if (age <= TeenBoy.MAX_AGE)
+            return new TeenBoy();
+        return new Man();
+    }
 }
