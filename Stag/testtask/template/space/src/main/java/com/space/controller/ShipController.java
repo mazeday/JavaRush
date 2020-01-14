c1760905e48564921ea50cab79af220c655ebb69
mazeday <mazeday54@gmail.com>
2020-01-14 22:12:25 +0700

    begin 3

--- Stag/testtask/template/space/src/main/java/com/space/controller/ShipController.java
+++ Stag/testtask/template/space/src/main/java/com/space/controller/ShipController.java
@@ -1,11 +1,17 @@
 package com.space.controller;
 
 
+import com.space.model.Ship;
 import com.space.repository.ShipRepository;
 import com.space.service.ShipService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ShipController {
@@ -19,13 +25,7 @@ public class ShipController {
     //CREATE
     @RequestMapping(value = "rest/ships", method = RequestMethod.POST)
     public ResponseEntity createShip(@RequestBody Ship ship) {
-        if (!shipService.isParamsNull(ship) && shipService.isParamsValid(ship)) {
-            ship.setIsUsed(ship.getIsUsed() == null ? false : ship.getIsUsed());
-            ship.setRating();
-            shipService.add(ship);
-            return new ResponseEntity(ship, HttpStatus.OK);
-        }
-        return new ResponseEntity(HttpStatus.BAD_REQUEST);
+
     }
 
 
