f571ca246dca20d5c6bafbfe009b309d0a7cb293
mazeday <mazeday54@gmail.com>
2020-01-17 00:06:22 +0700

    begin 4

--- Stag/testtask/template/space/src/main/java/com/space/repository/ShipRepository.java
+++ Stag/testtask/template/space/src/main/java/com/space/repository/ShipRepository.java
@@ -3,15 +3,20 @@ package com.space.repository;
 import com.space.model.Ship;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 
 @Repository
 public interface ShipRepository extends PagingAndSortingRepository<Ship, Long> {
 
     Page<Ship> findAll(Pageable pageable);
+
+    @Query(value = "SELECT * FROM ship", nativeQuery = true)
+    List<Ship> getAll();
 
 
 }
