c9acc1979ef9f1011e0f8cb9f28827515d0c84d9
mazeday <mazeday54@gmail.com>
2020-01-18 20:26:17 +0700

    begin 5

--- Stag/testtask/template/space/src/main/java/com/space/service/ShipServiceImp.java
+++ Stag/testtask/template/space/src/main/java/com/space/service/ShipServiceImp.java
@@ -1,11 +1,18 @@
 package com.space.service;
 
 import com.space.model.Ship;
+import com.space.model.ShipType;
+import org.springframework.data.domain.Pageable;
 
-import java.awt.print.Pageable;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface ShipServiceImp {
+
+    List<Ship> getAllShips(Optional<String> name, Optional<String> planet, Optional<ShipType> shipType, Optional<Long> after, Optional<Long> before,
+                           Optional<Double> minSpeed, Optional<Double> maxSpeed, Optional<Integer> minCrewSize, Optional<Integer> maxCrewSize,
+                           Optional<Boolean> isUsed, Optional<Double> minRating, Optional<Double> maxRating, Pageable pageable);
 
     Ship getById(long id);
     List<Ship> findAll(Pageable pageable);
